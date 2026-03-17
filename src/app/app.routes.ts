d591823525a0327d24375b74a2410897c61bd9df
Paulo Catto <paulohcatto@hotmail.com>
2026-03-17 20:53:07 -0300

    feat: Implement core e-commerce pages including shop, blog, product/post details, contact, and about us, along with shared footer and routing.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,9 +4,11 @@ import { HomeComponent } from './home/home.component';
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'shop', loadComponent: () => import('./pages/shop/shop').then(m => m.Shop) },
-  { path: 'blog', loadComponent: () => import('./pages/dummy/dummy').then(m => m.Dummy) },
+  { path: 'story', loadComponent: () => import('./pages/our-story/our-story').then(m => m.OurStoryComponent) },
+  { path: 'blog', loadComponent: () => import('./pages/blog/blog.component').then(m => m.BlogComponent) },
+  { path: 'contact', loadComponent: () => import('./pages/contact/contact').then(m => m.ContactComponent) },
   { path: 'product/:id', loadComponent: () => import('./pages/product-detail/product-detail').then(m => m.ProductDetail) },
   { path: 'category/:id', loadComponent: () => import('./pages/shop/shop').then(m => m.Shop) },
-  { path: 'post/:id', loadComponent: () => import('./pages/dummy/dummy').then(m => m.Dummy) },
-  { path: '**', redirectTo: '' },
+  { path: 'post/:id', loadComponent: () => import('./pages/post-detail/post-detail').then(m => m.PostDetail) },
+  { path: '**', loadComponent: () => import('./pages/dummy/dummy').then(m => m.Dummy) },
 ];
